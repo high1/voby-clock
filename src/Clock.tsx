82a3fcada4055ec8f3f05f7fa47cde2b32d8bbac
high1 <high1@mailfence.com>
2022-05-09 12:34:16 +0200

    Updates

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -14,15 +14,15 @@ type ClockFaceProps = {
 }
 
 const ClockFace = ({ hour, minute, second, subsecond }: ClockFaceProps): JSX.Element => (
-  <svg viewBox="0 0 200 200" width="95vh">
+  <svg viewBox="0 0 200 200" class="h-9/10">
     <g transform="translate(100, 100)">
       <circle class="text-neutral-900" r="99" fill="transparent" stroke="currentColor"/>
       <Lines numberOfLines={60} class='text-neutral-400' length={3} width={1} />
       <Lines numberOfLines={12} class='text-neutral-800' length={7} width={2} />
       <Hand rotate={subsecond} class="text-neutral-200 change-transform" length={83} width={5} />
-      <Hand rotate={hour} class="text-neutral-800 change-transform" length={50} width={4} />
-      <Hand rotate={minute} class="text-neutral-800 change-transform" length={70} width={3} />
-      <Hand rotate={second} class="text-red-500 change-transform" length={77} width={2} />
+      <Hand rotate={hour} class="text-neutral-800" length={50} width={4} />
+      <Hand rotate={minute} class="text-neutral-800" length={70} width={3} />
+      <Hand rotate={second} class="text-red-500" length={77} width={2} />
     </g>
   </svg>
 );
@@ -32,7 +32,7 @@ export const Clock = (): JSX.Element => {
   useAnimationLoop(() => time(getSecondsSinceMidnight()));
 
   const rotate = (rotate: number, fixed: number = 1) => `rotate(${(rotate * 360).toFixed(fixed)})`;
-  const subsecond = () => rotate(time() % 1);
+  const subsecond = () => rotate(time() % 1, 0);
   const second = () => rotate(time() % 60 / 60);
   const minute = () => rotate(time() / 60 % 60 / 60);
   const hour = () => rotate(time() / 60 / 60 % 12 / 12);
