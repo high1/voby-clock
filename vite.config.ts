f52769202c2763ba3fd9495c2156a7eba99c9ee2
high1 <high1@mailfence.com>
2022-08-07 23:02:58 +0200

    refactor: used new esbuild support for jsx
    - using automatic jsx runtime and setting jsxImportSource to voby
    enables compilation without needing a plugin, so vite-voby was removed
    from devDependencies
    - had to set options for esbuild in vite config, although those options
    were already set in tsconfig.json, so esbuild would work properly, if
    the options were passed to it
    - updated libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
-import voby from 'voby-vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/voby-clock/',
-  plugins: [voby(), uno(), tsconfigPaths()],
-  optimizeDeps: {
-    disabled: false,
-  },
   build: {
     commonjsOptions: { include: [] },
+  },
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'voby',
+  },
+  plugins: [uno(), tsconfigPaths()],
+  optimizeDeps: {
+    disabled: false,
   },
 });
