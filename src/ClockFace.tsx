78d98b86eb3d752eccc092007bf4257400bdefa3
high1 <high1@mailfence.com>
2022-10-12 00:00:49 +0200

    feat: code unification

--- src/ClockFace.tsx
+++ src/ClockFace.tsx
@@ -1,12 +1,12 @@
 import { $, useAnimationLoop } from 'voby';
-import { Hand } from 'Hand';
+import { ClockHand } from 'ClockHand';
 
-const getSecondsSinceMidnight = (): number =>
-  (Date.now() - new Date().setHours(0, 0, 0, 0)) / 1000;
+const length = 60;
 
 export const ClockFace = () => {
+  const getSecondsSinceMidnight = (): number =>
+    (Date.now() - new Date().setHours(0, 0, 0, 0)) / 1000;
   const time = $(getSecondsSinceMidnight());
-  useAnimationLoop(() => time(getSecondsSinceMidnight()));
 
   const rotate = (rotate: number, fractionDigits = 1) =>
     `rotate(${(rotate * 360).toFixed(fractionDigits)})`;
@@ -14,6 +14,8 @@ export const ClockFace = () => {
   const second = () => rotate((time() % 60) / 60);
   const minute = () => rotate(((time() / 60) % 60) / 60);
   const hour = () => rotate(((time() / 60 / 60) % 12) / 12);
+
+  useAnimationLoop(() => time(getSecondsSinceMidnight()));
 
   return (
     <div class="flex items-center justify-center h-full @dark:bg-neutral-700">
@@ -23,10 +25,10 @@ export const ClockFace = () => {
             class="stroke-neutral-900 @dark:stroke-neutral-100 fill-none"
             r="99"
           />
-          {Array.from({ length: 60 }, (_, index) => ({
+          {Array.from({ length }, (_, index) => ({
             isHour: index % 5 === 0,
-          })).map(({ isHour }, index, { length }) => (
-            <Hand
+          })).map(({ isHour }, index) => (
+            <ClockHand
               transform={rotate(index / length, 0)}
               class={
                 isHour
@@ -39,22 +41,22 @@ export const ClockFace = () => {
           ))}
         </g>
         <g class="translate-100px">
-          <Hand
+          <ClockHand
             transform={subsecond}
             class="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5 will-change-transform"
             length={83}
           />
-          <Hand
+          <ClockHand
             transform={hour}
             class="stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-4"
             length={50}
           />
-          <Hand
+          <ClockHand
             transform={minute}
             class="stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-3"
             length={70}
           />
-          <Hand
+          <ClockHand
             transform={second}
             class="stroke-red-500 stroke-width-2"
             length={77}
