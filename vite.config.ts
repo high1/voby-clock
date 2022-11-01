75f05de7b54ed8492554504676804eb12319b900
high1 <high1@users.noreply.github.com>
2022-11-01 10:33:04 +0100

    refactor(build): conditional configuration (#51)
    - simplified how the BASE is read from the environment slightly

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default ({ mode }: { mode: string }) => {
-  Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-
-  return defineConfig({
-    base: process.env['BASE'],
+export default ({ mode }: { mode: string }) =>
+  defineConfig({
+    base: loadEnv(mode, process.cwd(), '')['BASE'],
     build: { commonjsOptions: { include: [] } },
     // Vite 4 should remove the need to duplicate this from tsconfig
     esbuild: { jsx: 'automatic', jsxImportSource: 'voby' },
     optimizeDeps: { disabled: false },
     plugins: [uno(), tsconfigPaths()],
   });
-};
