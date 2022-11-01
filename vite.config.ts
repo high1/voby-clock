c9589fc9c4c014f3f945b068adc10fd073c65ca6
high1 <high1@mailfence.com>
2022-11-01 09:40:14 +0100

    fix: updated Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,10 @@ import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-<<<<<<< Updated upstream
-  base: '/voby-clock/',
-  build: {
-    commonjsOptions: {
-      include: [],
-    },
-  },
-  optimizeDeps: {
-    disabled: false,
-  },
-=======
   base: import.meta.env.BASE,
   build: { commonjsOptions: { include: [] } },
   // Vite 4 should remove the need to duplicate this from tsconfig
   esbuild: { jsx: 'automatic', jsxImportSource: 'voby' },
   optimizeDeps: { disabled: false },
->>>>>>> Stashed changes
   plugins: [uno(), tsconfigPaths()],
 });
