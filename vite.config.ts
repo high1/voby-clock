00164d7546e0b8103a0fe8e459a682145a590b7e
high1 <high1@mailfence.com>
2022-12-10 22:35:21 +0100

    feat: upgraded to Vite 4
    - Vite 4 is now used, so there's no need to set jsx and jsxImportSource
    for esbuild in vite.config.ts separately - TypeScript configuration is
    enough
    - removed html linting and formatted index.html with prettier
    - tsconfig is alson jsonc, so updated ESLint settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ export default ({ mode }: ConfigEnv) =>
   defineConfig({
     base: loadEnv(mode, process.cwd(), '')['BASE'],
     build: { commonjsOptions: { include: [] } },
-    // Vite 4 should remove the need to duplicate this from tsconfig
-    esbuild: { jsx: 'automatic', jsxImportSource: 'voby' },
     optimizeDeps: { disabled: false },
     plugins: [uno(), tsconfigPaths()],
   });
