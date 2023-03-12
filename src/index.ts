11c196838e66ad73124b2f09dfb7f89a8d553694
high1 <high1@users.noreply.github.com>
2023-03-12 13:45:33 +0100

    chore: cleanup and update (#79)
    - cleaned up atomic css classes more - stroke instead of stroke-width,
    used grid instead of flex, updated translate
    - cleaned up and updated colors
    - tweaked hands length
    - added editorconfig to prettier config
    - renamed vite-end to env
    - use plain initial index.ts file

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import 'uno.css';
 import { render } from 'voby';
 import { ClockFace } from 'ClockFace';
 
-render(<ClockFace />, document.querySelector('#root'));
+render(ClockFace, document.querySelector('#root'));
