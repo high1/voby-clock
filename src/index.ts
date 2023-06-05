a7e54aa79e186c0daaeed662ee2f9cf308fa2a74
high1 <high1@users.noreply.github.com>
2023-06-06 01:01:57 +0200

    Used common gray override (#94)
    * Used common gray override
    
    * Restored proper second color

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ render(
   hmr((cb: (mod: unknown) => void) => import.meta.hot?.accept(cb), ClockFace),
   document.querySelector('#root')
 );
+
+render(ClockFace, document.querySelector('#root'));
--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'unocss';
+import { defineConfig, presetUno } from 'unocss';
 
+const { theme } = presetUno();
 export default defineConfig({
   theme: {
     colors: {
+      gray: theme?.colors?.['zinc'] ?? {},
       voby: '#ffcb3d',
     },
   },
