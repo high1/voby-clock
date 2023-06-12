c0b7cc01b107c2d041e88c4da6400689c03e122b
high1 <high1@users.noreply.github.com>
2023-06-12 11:54:52 +0200

    Fixed HMR (#96)
    - use voby-vite in development for proper HMR
    - package updates

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import 'uno.css';
-import { hmr, render } from 'voby';
+import { render } from 'voby';
 import { ClockFace } from 'ClockFace';
-
-render(
-  hmr((cb: (mod: unknown) => void) => import.meta.hot?.accept(cb), ClockFace),
-  document.querySelector('#root')
-);
 
 render(ClockFace, document.querySelector('#root'));
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import voby from 'voby-vite';
 
 export default ({ mode }: ConfigEnv) =>
   defineConfig({
     base: loadEnv(mode, process.cwd(), '')['BASE'] ?? '',
-    plugins: [uno(), tsconfigPaths()],
+    plugins: [
+      uno(),
+      tsconfigPaths(),
+      mode === 'development' && voby({ hmr: { enabled: true } }),
+    ],
   });
